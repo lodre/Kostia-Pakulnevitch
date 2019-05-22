98f3cd6e0be46df2b10bbede5f206efbdc1b9707
lodre <pakulnevitchkostia@yandex.ru>
2019-05-22 16:55:37 +0300

    kostia

--- Работа на уроке 19.03.2019/n1.ml
+++
@@ -1,7 +0,0 @@
-type tree = Leaf | Node of tree * tree * int;;
-let rec make_tree n p = if n>0 then (let k = int_of_float (2.**((float n)-.1.)) in Node (make_tree (n-1) p,make_tree (n-1) (p+k),(k+p))) else Leaf;;
-let n = 3;;
-let t = make_tree n 0;;
-let rec s_of_t t = match t with Leaf->"leaf"
-                               |Node (t1,t2,i)->"Node ("^(s_of_t t1)^","^(s_of_t t2)^","^(string_of_int i)^")";;
-print_string (s_of_t t);; 
--- Работа на уроке 19.03.2019/n2.ml
+++
@@ -1,7 +0,0 @@
-type tree = Leaf | Node of tree * tree * int;;
-let rec make_bambuk n p = if p>0 then Node (Leaf,(make_bambuk n (p-1)),p) else Leaf;; 
-let n = 3;;
-let t = make_bambuk n n;;
-let rec s_of_t t = match t with Leaf->"leaf"
-                               |Node (t1,t2,i)->"Node ("^(s_of_t t1)^","^(s_of_t t2)^","^(string_of_int i)^")";;
-print_string (s_of_t t);; 
