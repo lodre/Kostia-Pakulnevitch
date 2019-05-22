de7366129d89e412bca26d6f8d399cfbfd720d8b
lodre <pakulnevitchkostia@yandex.ru>
2019-05-22 16:56:27 +0300

    kostia

--- Работа на уроке 24.04.2019/n1.ml
+++
@@ -1,7 +0,0 @@
-let l = [(0,325);(1,34);(2,45);(3,324)];;
-type 'a ty = B of 'a|N;;
-let a = 2;;      
-let rec key l a = match l with (x,y)::b->if x = a then B y else key b a
-                              |[]->N;;
-let b = key l a;;
-print_string (match b with N-> "No"|B b->string_of_int b);;
