ae8a1f6d9b64436be3d22ef71801c80a7353ef7b
lodre <pakulnevitchkostia@yandex.ru>
2019-05-22 16:45:44 +0300

    kostia

--- Контрольная Работа 1/n1.ml
+++
@@ -1,5 +0,0 @@
-type t = Br of t*t|L;;
-let rec s_of_t tree = match tree with Br (t1,t2)->"Br("^(s_of_t t1)^","^(s_of_t t2)^")"
-                                     |L->"L";;
-let tree = Br(Br(L,L),L);;
-print_string (s_of_t tree);;
--- Контрольная Работа 1/n4.ml
+++
@@ -1,6 +0,0 @@
-type t2 = B of t2 list;;                                 
-let rec map f l = match l with a::b->(f a);(map f b)|[]->();;
-let rec print_line n k = if k>0 then (print_string (String.make ((n+1)*k) ' '));;
-let rec f tree n k = match tree with B t->print_line n k;print_string "*\n";map (fun t -> f t n (k+1)) t;;
-let tree = B[B[B[];B[]];B[];B[]];;
-f tree 3 0;;
